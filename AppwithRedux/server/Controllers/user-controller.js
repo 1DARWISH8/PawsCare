bfdafb7696a7502f9939597d8d452761a195f5b7
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-01 23:05:41 +0530

    Create user-controller.js

---
+++ AppwithRedux/server/Controllers/user-controller.js
@@ -0,0 +1,12 @@
+//import models
+const {User,Admin,Product} = require('../db')
+// import bcryptjs for password hashing
+const bcryptjs = require('bcryptjs')
+// import jsonwebtokens for JWT
+const jwt = require('jsonwebtoken')
+// import cloudinary middleware for image and other fileupload
+
+// import fs (filesystem) module
+const fs = require('fs');
+
+module.exports={}
