ea3060b93eb44acb7fb19e79d65d00db22677d21
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-06 15:01:17 +0530

    Update Login.js

--- AppwithRedux/client/src/components/Login.js
+++ AppwithRedux/client/src/components/Login.js
@@ -1,31 +1,49 @@
-import React, { useContext,useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {NavLink, useNavigate} from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-// import { compareSync } from 'bcryptjs'
-// import axios from 'axios'
-import { userLoginContext } from '../contexts/userLoginContext'
-
+import { userLoginPromiseStatus } from '../redux/slices/userLoginSlice'
+import {useDispatch,useSelector} from 'react-redux'
 
 function Login() {
   let {register,handleSubmit,formState:{errors}}=useForm()
+  let dispatch = useDispatch()
   let navigate=useNavigate()
+  let {currentUser,loginStatus,errorMessage,isPending} = useSelector(state=>state.userLogin)
+  let [userType,setUserType] = useState('user')
 
-  let [,,userLoginStatus,,formSubmit,error]=useContext(userLoginContext)
+  let userTypeChange=(event)=>
+  {
+    setUserType(event.target.value)
+  }
+
+  const formSubmit=(data)=>
+  {
+    data={userType,...data}
+    dispatch(userLoginPromiseStatus(data))
+  }
 
   useEffect(()=>
   {
-      if(userLoginStatus===true)
+      if(loginStatus===true)
       {
         navigate('/home')
       }
-  },[userLoginStatus])
+  },[loginStatus])
 
   return (
     <div>
       <form className='col-sm-6 mx-auto m-3 p-3' onSubmit={handleSubmit(formSubmit)}>
         <h1 className='text-center fs-3 text-decoration-underline'>LOG IN</h1>
+      <label className='form-label fw-bold'>SELECT USER TYPE:</label>
+      <input type='radio' id='user' name='userType' value='user' checked={userType==='user'} onChange={userTypeChange}></input>
+      <label htmlFor='user'>USER</label>
+      <input type='radio' id='admin' name='userType' value='admin' onChange={userTypeChange}></input>
+      <label htmlFor='admin'>ADMIN</label>
+      <input type='radio' id='seller' name='userType' value='seller' onChange={userTypeChange} ></input>
+      <label htmlFor='seller'>SELLER</label>
+
         <p className='text-center fw-bold pt-1'>Not a USER?<NavLink to='/getstarted/register'>REGISTER</NavLink></p>
-        {error.length!==0&&<p className='fw-bold text-center text-danger'>{error}</p>}
+        {errorMessage.length!==0&&<p className='fw-bold text-center text-danger'>{errorMessage.message}</p>}
       <div className='sm-3 m-3 pt-3'>
         <label className='form-label fw-bold' htmlFor='username'>USERNAME</label>
         <input className='form-control border-black' type='text' id='username' {...register('username',{required:true})}></input>
@@ -37,7 +55,7 @@ function Login() {
         {errors.password?.type==='required'&&<p className='text-center text-danger fw-bold'>PASSWORD is REQUIRED</p>}
       </div>
       <div className='text-center'>
-        <button type='submit' className='btn btn-success border-black fw-bold'>SUBMIT</button>
+        <button type='submit' className='btn btn-success border-black fw-bold'>LOG IN</button>
       </div>
       </form>
     </div>
