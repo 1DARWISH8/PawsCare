b5834cbdc8c4b56690ff8a3adbea8124f6de3ee4
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-06 15:28:30 +0530

    Update user-api.js

--- AppwithRedux/server/APIs/user-api.js
+++ AppwithRedux/server/APIs/user-api.js
@@ -12,8 +12,27 @@ const {} = require('../Controllers/user-controller')
 const verifytoken = require('../Middlewares/verifytoken')
 // import upload and/(or) cloudinary configurations
 const {upload} = require('../Middlewares/cloudinaryUpload')
+// import cors and use
+const cors = require('cors')
+userApp.use(cors(
+    {
+        'Access-Control-Allow-Origin':'*'
+    }
+))
 
+// import req handlers from controllers
+const {getuser,registerUser,userLogin} = require('../Controllers/user-controller')
 
 // CRUD OPERATIONS
+
+// get user
+userApp.get('/getuser',expressAsyncHandler(getuser))
+
+
+// CREATE A USER IN USER COLLECTION OF PAWSCARE
+userApp.post('/registeruser',upload.single('pic'),expressAsyncHandler(registerUser))
+
+// USER LOGIN
+userApp.post('/login',expressAsyncHandler(userLogin))
 
 module.exports=userApp;
