7a816e8ca9e4d183c8d83f8a245b307fd82edc86
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-06 15:29:36 +0530

    Update admin-controller.js

--- AppwithRedux/server/Controllers/admin-controller.js
+++ AppwithRedux/server/Controllers/admin-controller.js
@@ -1,12 +1,19 @@
 //import models
-const {User,Admin,Product} = require('../db')
+const {User,Admin,Seller,Product} = require('../db')
 // import bcryptjs for password hashing
 const bcryptjs = require('bcryptjs')
 // import jsonwebtokens for JWT
 const jwt = require('jsonwebtoken')
 // import cloudinary middleware for image and other fileupload
-
+const cloudinary = require('../Middlewares/cloudinaryUpload')
 // import fs (filesystem) module
 const fs = require('fs');
 
-module.exports={}
+// get admins
+const getadmin = async(req,res)=>
+{
+    const admins = await Admin.find()
+    res.status(201).send({message:"ADMINS",payload:admins})
+}
+
+module.exports={getadmin}
