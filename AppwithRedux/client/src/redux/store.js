479d0f6e81cb58ea9404ef4e94bf160d67808f04
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-08 22:54:11 +0530

    Update store.js

--- AppwithRedux/client/src/redux/store.js
+++ AppwithRedux/client/src/redux/store.js
@@ -1,13 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userLoginSlice from './slices/userLoginSlice'
 import usercartSlice from './slices/usercartSlice'
+import userDetailsSlice from './slices/userDetailsSlice'
 
 export const store=configureStore(
     {
         reducer:
         {
             userLogin:userLoginSlice,
-            usercart:usercartSlice
+            usercart:usercartSlice,
+            userdetails:userDetailsSlice
         }
     }
 )
