d89c704899eeb59b8982d4caddac45f288abe00c
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-11 18:44:05 +0530

    Update Checkappointments.js

--- AppwithRedux/client/src/components/admin/Checkappointments.js
+++ AppwithRedux/client/src/components/admin/Checkappointments.js
@@ -54,16 +54,25 @@ function Checkappointments() {
         <form onSubmit={handleSubmit(formSubmit)}>
           <label htmlFor='service' className='fw-bold m-2'>SERVICE:</label>
           <select id='service' {...register('service',{required:true})}>
+          <option value=''>SELECT</option>
           <option value='HEALTH CHECK UP'>HEALTH CHECK UP</option>
           <option value='GROOMING'>GROOMING</option>
           <option value='TRAINING'>TRAINING</option>
           </select>
           <label htmlFor='location' className='fw-bold m-2'>LOCATION:</label>
           <select id='location' {...register('location',{required:true})}>
+          <option value=''>SELECT</option>
           <option value='BANGALORE'>BANGALORE</option>
           <option value='CHENNAI'>CHENNAI</option>
           <option value='DELHI'>DELHI</option>
           <option value='HYDERABAD'>HYDERABAD</option>
+          </select>
+          <label htmlFor='appointmentstatus' className='fw-bold m-2'>STATUS:</label>
+          <select id='appointmentstatus' {...register('appointmentstatus',{required:true})}>
+          <option value=''>SELECT</option>
+          <option value='COMPLETE'>COMPLETE</option>
+          <option value='PENDING'>PENDING</option>
+          <option value='CANCELLED'>CANCELLED</option>
           </select>
           <label className='fw-bold m-2' htmlFor='date'>SELECT DATE:</label>
           <input type='date' id='date' {...register('date',{required:true})}></input>
