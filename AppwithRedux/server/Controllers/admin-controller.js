b9e1717d69157ff74ef7f66b4736d8d736fffbff
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-11 18:47:25 +0530

    Update admin-controller.js

--- AppwithRedux/server/Controllers/admin-controller.js
+++ AppwithRedux/server/Controllers/admin-controller.js
@@ -14,8 +14,46 @@ const { Axios } = require('axios');
 const getadmin = async(req,res)=>
 {
     const admins = await Admin.find()
-    res.status(201).send({message:"ADMINS",payload:admins})
+    res.status(200).send({message:"ADMINS",payload:admins})
 }
+
+// get all users
+const getusers = async(req,res)=>
+{
+    const users = await User.find()
+    res.status(200).send({message:"USERS",payload:users})
+}
+
+// deactivate or ACTIVATE user
+const changeuserstatus = async(req,res)=>
+{
+    
+    try
+    {
+        let username = req.body.username
+        let account = req.body.accountstatus
+        let changed = await User.findOneAndUpdate({username:username},
+            {
+                $set:
+                {
+                    "accountstatus":account
+                }
+            },
+            {
+                returnOriginal:false
+            })
+        if (changed)
+        {
+            res.status(200).send({message:"USER ACCOUNT STATUS UPDATED",payload:changed})
+        }
+    }
+    catch(err)
+    {
+        res.status(200).send(err.message)
+    }
+}
+
+
 // get all appointments
 const getallappointments = async(req,res)=>
 {
@@ -67,7 +105,8 @@ const pendingappointment = async(req,res)=>
         let service = req.body.service
         let location = req.body.location
         let date = req.body.date
-        let appointments = await Appointment.find({service:service,location:location,date:date,appointmentstatus:"PENDING"})
+        let status = req.body.appointmentstatus
+        let appointments = await Appointment.find({service:service,location:location,date:date,appointmentstatus:status})
         if (appointments)
         {
             res.status(200).send({message:"APPOINTMENTS",payload:appointments})
@@ -105,4 +144,4 @@ const cancelledappointments = async(req,res)=>
     }
 }
 
-module.exports={getadmin,getallappointments,pendingappointments,pendingappointment,cancelledappointments}
+module.exports={getadmin,getusers,changeuserstatus,getallappointments,pendingappointments,pendingappointment,cancelledappointments}
