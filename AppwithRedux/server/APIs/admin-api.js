e26ae24307ab9c942358431b0f02ce3d70753917
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-12 16:48:21 +0530

    Update admin-api.js

--- AppwithRedux/server/APIs/admin-api.js
+++ AppwithRedux/server/APIs/admin-api.js
@@ -7,7 +7,7 @@ const adminApp = express.Router()
 // import express-async-handler to handle async errors (error handler in server.js cannot handle async errors)
 const expressAsyncHandler = require('express-async-handler')
 // import req handlers from controllers
-const {getadmin,getusers,changeuserstatus,getallappointments,pendingappointments,pendingappointment,cancelledappointments} = require('../Controllers/admin-controller')
+const {getadmin,getusers,changeuserstatus,getallappointments,pendingappointments,pendingappointment,cancelledappointments,getproducts,addproduct,getaproduct,editproduct,deactivateproduct,activateproduct,inactiveproducts} = require('../Controllers/admin-controller')
 // import token verification middleware
 const verifytoken = require('../Middlewares/verifytoken')
 // import upload and/(or) cloudinary configurations
@@ -44,5 +44,25 @@ adminApp.post('/pendingappointment',expressAsyncHandler(pendingappointment))
 
 // GET CANCELLED APPOINTMENTS
 adminApp.get('/cancelledappointments',expressAsyncHandler(cancelledappointments))
+
+// GET PRODUCTS
+adminApp.get('/getproducts',expressAsyncHandler(getproducts))
+
+// ADD PRODUCT
+adminApp.post('/addproduct',upload.single('image'),expressAsyncHandler(addproduct))
+
+// EDIT PRODUCT
+// get the exact product details
+adminApp.post('/getaproduct',expressAsyncHandler(getaproduct))
+adminApp.post('/editproduct',upload.single('image',expressAsyncHandler(editproduct)))
+
+// DELETE A PRODUCT BY DEACTIVATING IT
+adminApp.post('/deactivateproduct',expressAsyncHandler(deactivateproduct))
+
+// RESTORE A PRODUCT BY ACTIVATING IT
+adminApp.post('/activateproduct',expressAsyncHandler(activateproduct))
+
+// GET INACTIVE PRODUCTS
+adminApp.get('/inactiveproducts',expressAsyncHandler(inactiveproducts))
 
 module.exports=adminApp;
