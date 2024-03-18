517e296f768d3ab1e4a026de4d71cc8d656cba9a
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-18 19:22:39 +0530

    Update Store.js

--- AppwithRedux/client/src/components/Store.js
+++ AppwithRedux/client/src/components/Store.js
@@ -1,45 +1,90 @@
-import React, {useContext} from 'react'
-import { NavLink,Outlet,useNavigate } from 'react-router-dom'
-import { userLoginContext } from '../contexts/userLoginContext'
+import React, {useEffect,useState} from 'react'
+import { NavLink,useNavigate } from 'react-router-dom'
+import {useDispatch,useSelector} from 'react-redux'
+import axios from 'axios'
+import { productDetailsPromiseStatus } from '../redux/slices/productDetailsSlice' 
 
 function Store() {
 
-  let [,,userLoginStatus,,,,cart]=useContext(userLoginContext)
   let navigate = useNavigate()
+  let [products,setProducts]=useState([])
+  let [error,setError] =useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   function tocart()
   {
-    if(userLoginStatus)
+    // if()
+    // {
+    //   navigate('/store/cart')
+    // }
+    // else
+    // {
+    //   alert("SIGN UP / LOG IN TO ORDER")
+    //   navigate('/getstarted')
+    // }
+  }
+
+  async function viewproducts()
+  {
+    try
     {
-      navigate('/store/cart')
+      let products = await axios.get('http://localhost:5000/user-api/getproducts')
+      if (products)
+      {
+        setProducts(products.data.payload)
+        setSearchResults(products.data.payload)
+        // console.log(products.data.payload)
+      }
     }
-    else
+    catch(err)
     {
-      alert("SIGN UP / LOG IN TO ORDER")
-      navigate('/getstarted')
+      setError(err.message)
     }
   }
 
+  useEffect(()=>viewproducts,[])
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    const results = products.filter(product =>
+    product.productname.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setSearchResults(results);
+};
+
   return (
     <div>
-        <div className="nav justify-content-center rounded p-2">
-        <li className='nav-item'>
-            <NavLink className='nav-link border rounded bg-secondary text-white' id='nav-link' to='/store/food'>FOOD</NavLink>
-        </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link border rounded bg-secondary text-white' to='/store/treats'>TREATS</NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link border rounded bg-secondary text-white' to='/store/toys'>TOYS</NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link border rounded bg-secondary text-white' to='/store/essentials'>ESSENTIALS</NavLink>
-          </li>
-          <li className='nav-item'>
-            <button className='btn m-1' onClick={tocart}><span className='cart'>CART<i className="bi bi-cart4"></i><span className='number'>{cart.length}</span></span></button>
-          </li>
+
+      <div className='text-center'>
+                <input
+            type="text"
+            placeholder="Search by product name"
+            value={searchTerm}
+            onChange={handleChange}
+        />
       </div>
-      <Outlet/>
+
+      <div className='container pt-2'>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-4'>
+            {searchResults.map((item,index)=>
+            (
+                <div className='col pt-3 pb-3'  key={index}>
+                    <div className='card' style={{height:'500px'}}>
+                        <img src={item.image} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name}/>
+                        <div className='card-body'>
+                            <h5 className='card-title'>{item.productname}</h5>
+                            <p className='card-text'>Rs.{item.price}</p>
+                            <span>
+                            <NavLink className='btn btn-success' >ADD TO CART</NavLink>    
+                            <button className='btn btn-danger' >HEART</button>    
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            </div>
+        </div>
     </div>
   )
 }
