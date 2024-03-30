19c0a648bad4a7c632883ccf8622d2de534764a2
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-03-30 12:50:18 +0530

    Update Register.js

--- AppwithRedux/client/src/components/Register.js
+++ AppwithRedux/client/src/components/Register.js
@@ -1,130 +1,235 @@
-import React, {useEffect,useState} from 'react'
-import { NavLink,useNavigate } from 'react-router-dom'
-import {useDispatch,useSelector} from 'react-redux'
-import axios from 'axios'
-import { productDetailsPromiseStatus } from '../redux/slices/productDetailsSlice' 
-import {Alert} from 'react-bootstrap';
-
-function Store() {
-
-  let navigate = useNavigate()
-  let [products,setProducts]=useState([])
-  let [error,setError] =useState('')
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  let {currentUser}=useSelector(state=>state.userLogin)
-  let [alert,setAlert] = useState('')
+import React, {useState} from 'react'
+import {useForm} from "react-hook-form"
+import axios from 'axios';
+import {NavLink, useNavigate} from 'react-router-dom'
+import Alert from 'react-bootstrap/Alert';
 
 
-  function tocart()
-  {
-    // if()
-    // {
-    //   navigate('/cart')
-    // }
-    // else
-    // {
-    //   alert("SIGN UP / LOG IN TO ORDER")
-    //   navigate('/getstarted')
-    // }
-  }
+function Register() {
 
-  async function viewproducts()
-  {
-    try
+    let {register,handleSubmit,setValue,formState:{errors}}=useForm()
+    let navigate=useNavigate();
+    let [error,setError]=useState('')
+    let [file,setFile]=useState(null)
+    const [userType,setUserType]=useState('user')
+
+    const [selectedState, setSelectedState] = useState('');
+
+    const indianStates = [
+    { name: 'Andhra Pradesh', districts: ['Anantapur', 'Chittoor', 'East Godavari', 'Guntur', 'Krishna', 'Kurnool', 'Prakasam', 'Srikakulam', 'Visakhapatnam', 'Vizianagaram', 'West Godavari', 'Y.S.R. Kadapa'] },
+    { name: 'Karnataka', districts: ['Bagalkot', 'Ballari', 'Belagavi', 'Bengaluru Rural', 'Bengaluru Urban', 'Bidar', 'Chamarajanagar', 'Chikkaballapur', 'Chikkamagaluru', 'Chitradurga', 'Dakshina Kannada', 'Davanagere', 'Dharwad', 'Gadag', 'Hassan', 'Haveri', 'Kalaburagi', 'Kodagu', 'Kolar', 'Koppal', 'Mandya', 'Mysuru', 'Raichur', 'Ramanagara', 'Shivamogga', 'Tumakuru', 'Udupi', 'Uttara Kannada', 'Vijayapura', 'Yadgir'] },
+    { name: 'Kerala', districts: ['Alappuzha', 'Ernakulam', 'Idukki', 'Kannur', 'Kasaragod', 'Kollam', 'Kottayam', 'Kozhikode', 'Malappuram', 'Palakkad', 'Pathanamthitta', 'Thiruvananthapuram', 'Thrissur', 'Wayanad'] },
+    { name: 'Tamil Nadu', districts: ['Ariyalur', 'Chengalpattu', 'Chennai', 'Coimbatore', 'Cuddalore', 'Dharmapuri', 'Dindigul', 'Erode', 'Kallakurichi', 'Kancheepuram', 'Kanyakumari', 'Karur', 'Krishnagiri', 'Madurai', 'Nagapattinam', 'Namakkal', 'Nilgiris', 'Perambalur', 'Pudukkottai', 'Ramanathapuram', 'Ranipet', 'Salem', 'Sivaganga', 'Tenkasi', 'Thanjavur', 'Theni', 'Thoothukudi', 'Tiruchirappalli', 'Tirunelveli', 'Tirupathur', 'Tiruppur', 'Tiruvallur', 'Tiruvannamalai', 'Tiruvarur', 'Vellore', 'Viluppuram', 'Virudhunagar'] },
+    { name: 'Telangana', districts: ['Adilabad', 'Bhadradri Kothagudem', 'Hyderabad', 'Jagtial', 'Jangaon', 'Jayashankar Bhupalpally', 'Jogulamba Gadwal', 'Kamareddy', 'Karimnagar', 'Khammam', 'Komaram Bheem', 'Mahabubabad', 'Mahabubnagar', 'Mancherial', 'Medak', 'Medchal–Malkajgiri', 'Mulugu', 'Nagarkurnool', 'Nalgonda', 'Narayanpet', 'Nirmal', 'Nizamabad', 'Peddapalli', 'Rajanna Sircilla', 'Ranga Reddy', 'Sangareddy', 'Siddipet', 'Suryapet', 'Vikarabad', 'Wanaparthy', 'Warangal Rural', 'Warangal Urban', 'Yadadri Bhuvanagiri'] }];
+ 
+  const handleStateChange = (e) => {
+    const selectedState = e.target.value;
+    setSelectedState(selectedState);
+    // Clear the city field when state changes
+    setValue('city', '');
+  };
+
+    let userTypeChange=(event)=>
     {
-      let products = await axios.get('http://localhost:5000/user-api/getproducts')
-      if (products)
-      {
-        setProducts(products.data.payload)
-        setSearchResults(products.data.payload)
-        // console.log(products.data.payload)
-      }
+        setUserType(event.target.value)
     }
-    catch(err)
+
+    function uploadPic(e)
     {
-      setError(err.message)
+        setFile(e.target.files[0])
     }
-  }
 
-  useEffect(()=>viewproducts,[])
-
-  const hideAlert = () =>
-  {
-    setTimeout(()=>
+    async function formSubmit(data)
     {
-      setAlert('');
-    },5000);
-  }
-
-  useState(()=>
-  {
-    hideAlert();
-  },[])
-
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-    const results = products.filter(product =>
-    product.productname.toLowerCase().includes(e.target.value.toLowerCase())
-    );
-    setSearchResults(results);
-};
-
-  async function addtocart(item)
-  {
-    try
-    {
-        let username=currentUser.username;
-        item = {...item,username}
-        let added = await axios.post('http://localhost:5000/user-api/addcartproduct',item)
-        console.log(added)
-        if (added.data.message === "PRODUCT ADDED TO CART")
+        // store in local api
+        data={userType,...data}
+        // console.log(data)
+        const formData = new FormData();
+        formData.append('data',JSON.stringify(data))
+        // if(userType==='user')
+        // {
+            //     formData.append('userpic',file)
+        //     formData.append('petpic',file)
+        
+        // }
+        // else
+        formData.append('userpic',file)
+        // {
+        // }
+        try
         {
-          setAlert('PRODUCT ADDED')
+            let res = await axios.post('http://localhost:5000/user-api/registeruser',formData)
+            // console.log(res)
+            if (res.status===201)
+            {
+                navigate('/getstarted/login')
+            }
+            else
+            {
+                setError(res.data.message)
+            }
+        }
+        catch(err)
+        {
+            setError(err.message)
         }
     }
-    catch(err)
+
+    // console.log(errors)
+
+    function Back()
     {
-      setError(err.message)
+        navigate('/getstarted')
     }
-  }
 
-  return (
+
+return (
     <div>
+        <form className='col-sm-6 mx-auto m-3 p-2' onSubmit={handleSubmit(formSubmit)}>
+        <h1 className='text-center fs-3 text-decoration-underline'>REGISTRATION</h1>
+        <p className='text-center fw-bold'>ALREADY A USER?<NavLink to='/getstarted/login'>Login</NavLink></p>
+        {error.length!==0&& <p className='fw-bold text-center text-danger border-0'>{error}</p>}
 
-      <div className='text-center'>
-                <input
-            type="text"
-            placeholder="Search by product name"
-            value={searchTerm}
-            onChange={handleChange}
-        />
-      </div>
+        <label className='form-label fw-bold'>SELECT USER TYPE:</label>
+        <input type='radio' id='user' name='userType' value='user' checked={userType==='user'} onChange={userTypeChange}></input>
+        <label htmlFor='user'>USER</label>
+        <input type='radio' id='admin' name='userType' value='admin' onChange={userTypeChange}></input>
+        <label htmlFor='admin'>ADMIN</label>
+        {/* <input type='radio' id='seller' name='userType' value='seller' onChange={userTypeChange}></input>
+        <label htmlFor='seller'>SELLER</label> */}
 
-      {error.length!==0&& <p className='fw-bold text-center text-danger border-0'>{error}</p>}
-      {alert.length!==0 && <Alert variant={'dark'} onClose={()=>setAlert('')}>PRODUCT ADDED</Alert> }
-      <div className='container pt-2'>
-            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-4'>
-            {searchResults.map((item,index)=>
-            (
-                <div className='col pt-3 pb-3'  key={index}>
-                    <div className='card' style={{height:'500px'}}>
-                        <img src={item.image} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name}/>
-                        <div className='card-body'>
-                            <h5 className='card-title'>{item.productname}</h5>
-                            <p className='card-text'>Rs.{item.price}</p>
-                            <span>
-                            <NavLink className='btn btn-success' onClick={()=>addtocart(item)}>ADD TO CART</NavLink>    
-                            <button className='btn btn-danger' >HEART</button>    
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            ))}
+        {/* USER DETAILS FORM */}
+        <h5 className='text-center fw-bold pt-5'>USER DETAILS</h5>
+        <div className='sm-3 m-3'>
+            {/* username */}
+            <label htmlFor='username' className='form-label fw-bold' >USERNAME:</label>
+            <input type='text' className='form-control border-black' id='username' {...register('username',{required:true})}></input>
+            {errors.username?.type==='required'&&<p className='text-danger fw-bold text-center'>*USERNAME is required*</p>}
+        </div>
+        
+        <div className='sm-3 m-3'>
+            {/* password */}
+            <label htmlFor='password' className='form-label fw-bold' >PASSWORD:</label>
+            <input type='password' className='form-control border-black' id='password' {...register('password',{required:true})}></input>
+            {errors.password?.type==='required'&&<p className='text-center text-danger fw-bold'>*PASSWORD is required*</p>}
+        </div>
+        <div className='sm-3 m-3'>
+            {/* email */}
+            <label htmlFor='email' className='form-label fw-bold' >EMAIL ADDRESS:</label>
+            <input type='email' id='email' className='form-control border-black' {...register('email',{required:true})}></input>
+            {errors.email?.type==='required'&&<p className='text-center text-danger fw-bold'>*EMAIL is required*</p>}
+        </div>
+        <div className='sm-3 m-3'>
+            {/* USER DISPLAY PICTURE */}
+            <label htmlFor='userpic' className='form-label fw-bold'>PROFILE IMAGE:</label>
+            <input id='userpic' type='file' name='userpic' className='form-control border-black' onChange={uploadPic}/>
+        </div>
+
+
+
+        {userType==='user'&&
+        <>
+        {/* PET DETAILS FORM */}
+        <div >
+            <h5 className='text-center fw-bold pt-3'>PET DETAILS</h5>
+        {/* <div className='sm-3 m-3 '>
+        <label htmlFor='petpic' className=' form-label fw-bold'>UPLOAD PET IMAGE:</label> */}
+        {/* <input type='file' accept='image/*' id='imageupload' className='form-control' {...register('imageupload')}></input> */}
+        {/* <input type='file' id='petpic' className='form-control border-black' onChange={uploadPic}></input>
+        </div> */}
+        <div className='sm-3 m-3'>
+            <label htmlFor='petname' className='form-label fw-bold' >PETNAME:</label>
+            <input type='text' className='form-control border-black' id='petname' {...register('petdetails.petname',{required:true})}></input>
+            {errors.petdetails?.petname?.type==='required'&&<p className='text-danger fw-bold text-center'>*PETNAME is required*</p>}
+        </div>
+        <div className='sm-3 m-3'>
+            {/* DOB */}
+            <label htmlFor='dob' className='form-label fw-bold'>DATE OF BIRTH:</label>
+            <input type='date' id="dob" className='form-control border-black text-center' {...register('petdetails.dob',{required:true})}></input>
+            {errors.petdetails?.dob?.type==='required'&&<p className='text-center text-danger fw-bold'>*DATE OF BIRTH is required*</p>}
+        </div>
+        <div className='sm-3 m-3'>
+            <label className='form-label fw-bold' htmlFor='petanimal'>PET ANIMAL:</label>
+            <select className='form-control text-center border-black' id='petanimal' {...register('petdetails.petanimal',{required:true})}>
+                <option value="">--SELECT--</option>
+                <option value="DOG">DOG</option>
+                <option value="CAT">CAT</option>
+                <option value="BIRD">BIRD</option>
+                <option value="HAMSTER">HAMSTER</option>
+                <option value="OTHER">Other</option>
+            </select>
+            {errors.petdetails?.petanimal?.type==='required'&&<p className='text-center text-danger fw-bold'>*PET ANIMAL TYPE is required*</p>}
+        </div>
+        <div className='sm-3 m-3'>
+            {/* Previous Health checkup date */}
+            <label htmlFor='last_checkup_date' className='form-label fw-bold'>PREVIOUS DATE OF HEALTH CHECK-UP:</label>
+            <input type='date' id="last_checkup_date" className='form-control border-black text-center' {...register('petdetails.last_checkup_date',{required:true})}></input>
+            {errors.petdetails?.last_checkup_date?.type==='required'&&<p className='text-center text-danger fw-bold'>*PREVIOUS HEALTH CHECK-UP DATE is required*</p>}
+        </div>
+        </div>
+
+
+{/* COMMUNICATION DETAILS */}
+        <div>
+            <h5 className='text-center fw-bold pt-3'>COMMUNICATION DETAILS</h5>
+            <div className='sm-3 m-3 '>
+                <label className='form-label fw-bold' htmlFor='phonenumber'>PHONE NUMBER:</label>
+                <input className='form-control border-black' type='tele' {...register('phonenumber',{required:true,minLength:10,maxLength:10})}></input>
+                {errors.phonenumber?.type==='required'&&<p className='fw-bold text-danger text-center'>*PHONE NUMBER is required*</p>}
+            </div>
+
+{/* ADDRESS DETAILS */}
+            <div className='sm-3 m-3 '>
+            <h5 className='text-center fw-bold pt-3'>ADDRESS DETAILS</h5>
+                <label className='form-label fw-bold' htmlFor='state'>STATE:</label>
+                <select className='form-control border-black' id="state" {...register("address.state",{required:true})} onChange={handleStateChange}>
+                    <option value="">Select State</option>
+                        {indianStates.map((state, index) => (
+                            <option value={state.name}>
+                                    {state.name}
+                            </option>
+                        ))}
+                </select>
+                {errors.address?.state?.type==='required'&&<p className='fw-bold text-danger text-center'>*ADDRESS is required*</p>}
+            </div>
+
+            <div className='sm-3 m-3 '>
+                <label className='form-label fw-bold' htmlFor='district'>DISTRICT:</label>
+                {/* <textarea className='form-control border-black' type='text' id='address' {...register('address.addressline',{required:true})}></textarea> */}
+                <select className='form-control border-black' id="district" {...register("address.district")}>
+                    <option value="">Select City</option>
+                        {selectedState &&
+                            indianStates.find((state) => state.name === selectedState).districts.map((city, index) => (
+                    <option key={index} value={city}>
+                        {city}
+                    </option>
+                    ))}
+                </select>
+                {errors.address?.district?.type==='required'&&<p className='fw-bold text-danger text-center'>*ADDRESS is required*</p>}
+            </div>
+
+            <div className='sm-3 m-3 '>
+                <label className='form-label fw-bold' htmlFor='street'>ADDRESS LINE:</label>
+                <textarea className='form-control border-black' type='text' id='address' {...register('address.addressline',{required:true})}></textarea>
+                {errors.address?.addressline?.type==='required'&&<p className='fw-bold text-danger text-center'>*ADDRESS is required*</p>}
+            </div>
+            <div className='sm-3 m-3'>
+                <label className='form-label fw-bold' htmlFor='pincode'>PINCODE</label>
+                <input className='form-control border-black' type='number' id='pincode' {...register('address.pincode',{required:true})}></input>
+                {errors.address?.pincode?.type==='required'&&<p className='text-danger text-center fw-bold'>*PINCODE is required*</p>}
             </div>
         </div>
+        </>
+        }
+
+        <div className='text-center p-2'>
+            <button type='submit' className='btn btn-dark'>REGISTER</button>
+        </div>
+
+        </form>
+
     </div>
-  )
+)
 }
 
-export default Store
+export default Register
