299a64947bdcaee1c3a0a5dbc1b5d631ce9ca1be
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-08 23:58:13 +0530

    Update Checkappointments.js

--- AppwithRedux/client/src/components/admin/Checkappointments.js
+++ AppwithRedux/client/src/components/admin/Checkappointments.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import DatePicker from 'react-datepicker';
 import Accordion from 'react-bootstrap/Accordion';
+import '../Myappointments.css'
 
 function Checkappointments() {
   let {register,handleSubmit,formState:{errors}}=useForm()
@@ -23,6 +24,22 @@ function Checkappointments() {
   const today = new Date();
   const [selectedAppointment, setSelectedAppointment] = useState(null);
   const [timeslots,setTimeslots]=useState([])
+  const [sortedAppointments, setSortedAppointments] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc')
+  
+      // Function to handle sorting appointments data based on the selected sort order
+      function handleSort() {
+        const sorted = [...appointmentsdata].sort((a, b) => {
+          const dateA = new Date(a.appointment_date);
+          const dateB = new Date(b.appointment_date);
+          return sortOrder === 'asc' ? dateA - dateB : dateB - dateA;
+        });
+        setSortedAppointments(sorted);
+      }
+    
+      useEffect(() => {
+        handleSort();
+      }, [sortOrder, appointmentsdata]);
 
 
   const hideAlert = () =>
@@ -152,73 +169,88 @@ function Checkappointments() {
     <div className='text-center'>
         {error.length!==0&& <p className='fw-bold text-center text-danger border-0'>{error}</p>}
         {alert.length!==0 && <Alert variant={'dark'} onClose={()=>setAlert('')}>{alert}</Alert> }
-      {/* <span >
-        <form onSubmit={handleSubmit(formSubmit)}>
-          <label htmlFor='service' className='fw-bold m-2'>SERVICE:</label>
-          <select id='service' {...register('service',{required:true})}>
-          <option value=''>SELECT</option>
-          <option value='HEALTH CHECK UP'>HEALTH CHECK UP</option>
-          <option value='GROOMING'>GROOMING</option>
-          <option value='TRAINING'>TRAINING</option>
-          </select>
-          <label htmlFor='location' className='fw-bold m-2'>LOCATION:</label>
-          <select id='location' {...register('location',{required:true})}>
-          <option value=''>SELECT</option>
-          <option value='BANGALORE'>BANGALORE</option>
-          <option value='CHENNAI'>CHENNAI</option>
-          <option value='DELHI'>DELHI</option>
-          <option value='HYDERABAD'>HYDERABAD</option>
-          </select>
-          <label htmlFor='appointmentstatus' className='fw-bold m-2'>STATUS:</label>
-          <select id='appointmentstatus' {...register('appointmentstatus',{required:true})}>
-          <option value=''>SELECT</option>
-          <option value='COMPLETE'>COMPLETE</option>
-          <option value='PENDING'>PENDING</option>
-          <option value='CANCELLED'>CANCELLED</option>
-          </select>
-          <label className='fw-bold m-2' htmlFor='date'>SELECT DATE:</label>
-          <input type='date' id='date' {...register('date',{required:true})}></input>
-          <button className='btn btn-secondary m-2' type='submit'>Search</button>
-        </form>
-      </span> */}
+      
 
-      {appointmentsdata.length?
-      <>
-        <table>
-            <tbody>
-                {
-                    appointmentsdata.map((appointment,index)=>(
-                        <tr key={index}>
-                            <Card className='m-3'>
-                                <Card.Body>
-                                    <Card.Title>
-                                        SERVICE:{appointment.service}
-                                        <span>
-                                            {(appointment.appointment_status!=="CANCELLED" && appointment.appointment_status!=="COMPLETED")?
+
+
+{/* Sort selection dropdown */}
+            <div className="mx-5 text-start">
+                <label htmlFor="sortOrder" className="m-2">Sort by:</label>
+                <select id="sortOrder" className="" value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+                    <option value="asc">Oldest to Newest</option>
+                    <option value="desc">Newest to Oldest</option>
+                </select>
+                </div>
+    {sortedAppointments.length>0?
+        <>
+            {
+                sortedAppointments.map((appointment,index)=>
+                (
+                    <article class="postcard light red m-4">
+                        {
+                            appointment.appointment_service==="HEALTH CHECK UP"&&
+                            <img class="postcard__img" src="https://res.cloudinary.com/dozacgfl7/image/upload/v1712573019/Health_checkup_docs_itqetq.png" alt="Image Title" />	
+                        }
+                        {
+                            appointment.appointment_service==="GROOMING"&&
+                            <img class="postcard__img" src="https://res.cloudinary.com/dozacgfl7/image/upload/v1712573257/dog_training_eskdcl.jpg" alt="Image Title" />
+                        }
+                        {
+                            appointment.appointment_service==="TRAINING"&&
+                            <img class="postcard__img" src="https://res.cloudinary.com/dozacgfl7/image/upload/v1712573466/cat_grooming_bskql7.jpg" alt="Image Title" />	
+                        }
+                        <div class="postcard__text t-dark">
+                            <h1 class="postcard__title red">{appointment.appointment_service}</h1>
+                            <div class="postcard__subtitle small">
+                                {/* <time datetime="2020-05-25 12:00:00"> */}
+                                    <div className='text-start'>
+                                        <i class="fas fa-calendar-alt"></i><span className='fw-bold m-2'>APPOINTMENT DATE:</span>{appointment.appointment_date}
+                                    </div>
+                                    <div className='text-start'>
+                                        <i class="fas fa-clock "></i><span className='fw-bold m-2'>APPOINTMENT TIME:</span>{appointment.appointment_time}
+                                    </div>
+                                    <div className='text-start'>
+                                        <i class="fas fa-map-marker-alt "></i><span className='fw-bold m-2'>APPOINTMENT LOCATION:</span>{appointment.appointment_location},PawsCare
+                                    </div>
+                                {/* </time> */}
+                            </div>
+                            <div class="postcard__bar"></div>
+                            <div class="postcard__preview-txt">
+                            STATUS:
+                                {appointment.appointment_status === "CANCELLED" && <span className='text-danger fw-bold m-2'>{appointment.appointment_status}</span>}
+                                {appointment.appointment_status === "COMPLETED" && <span className='text-success fw-bold m-2'>{appointment.appointment_status}</span>}
+                                {(appointment.appointment_status !== "COMPLETED" && appointment.appointment_status !== "CANCELLED") && <span className='text-primary fw-bold m-2'>{appointment.appointment_status}</span>}
+                            </div>
+                            <div class="postcard__preview-txt">
+                            BOOKED BY:
+                                <span className='fw-bold m-2'>
+                                    {appointment.username}
+                                </span> 
+                            </div>
+                            <div class="postcard__preview-txt">
+                            PET NAME:
+                                <span className='fw-bold m-2'>
+                                    {appointment.petname}
+                                </span> 
+                            </div>
+                            <ul class="postcard__tagbox">
+                            {appointment.appointment_status==="PENDING"&&
                                             <>
-                                            <button className='btn btn-danger m-2' onClick={()=>cancelappointment(appointment)}>CANCEL</button>
+                                            <button className='btn btn-danger m-2' onClick={()=>cancelappointment(appointment)}>CANCEL APPOINTMENT</button>
+                                            <button className='btn btn-primary m-2' onClick={() => handleRescheduleClick(appointment)}>RESCHEDULE APPOINTMENT</button>
                                             <button className='btn btn-success m-2' onClick={()=>appointmentcompleted(appointment)}>COMPLETED</button>
-                                            <button className='btn btn-primary m-2' onClick={() => handleRescheduleClick(appointment)}>RESCHEDULE</button>
-                                            </>:
-                                            <></>
-                                            }
-                                        </span>
-                                        USER:{appointment.username}
-                                    </Card.Title>
-                                    <Card.Text>
-                                      STATUS:
-                                      {appointment.appointment_status === "CANCELLED" && <span className='text-danger'>{appointment.appointment_status}</span>}
-                                      {appointment.appointment_status === "COMPLETED" && <span className='text-success'>{appointment.appointment_status}</span>}
-                                      {(appointment.appointment_status !== "COMPLETED" && appointment.appointment_status !== "CANCELLED") && <span className='text-primary'>{appointment.appointment_status}</span>}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>APPOINTMENT LOCATION:{appointment.appointment_location},DATE&TIME:{appointment.appointment_date}&{appointment.appointment_time}</Card.Footer>
-                            </Card>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+                                            </>
+                            }
+                                {/* <li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+                                <li class="tag__item play blue"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+                                <li class="tag__item play red">
+                                    <a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+                                </li> */}
+                            </ul>
+                        </div>
+                    </article>
+                ))
+            }
       </>:
       <>
       <h5>NO APPOINTMENTS TO SHOW</h5>
