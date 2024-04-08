f83e7b90547d2ac6ef6be1b68b3b91953e7cfb9a
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-08 23:52:26 +0530

    Update Profile.js

--- AppwithRedux/client/src/components/Profile.js
+++ AppwithRedux/client/src/components/Profile.js
@@ -25,17 +25,21 @@ return (
 			userType==='user'?
 			<>
 			
-        <div className="container pt-3">
+        <div className="pt-3">
 		<div className="main-body">
 			<div className="row">
 				<div className="col-lg-4">
-					<div className="card m-3">
+					<div className="car m-3">
 						<div className="card-body">
 							<div className="d-flex flex-column align-items-center text-center">
-								{/* <img src={currentUser.imageupload} alt={currentUser.petanimal} className="rounded-circle p-1 bg-primary" width="150"/> */}
-								<div className="mt-3">
-									<h4>{currentUser.username}</h4>
-									<p className="text-black mb-1">Owner of {currentUser.petdetails[0].petname}</p>
+								{
+									currentUser.profileImageURL&&
+									<img src={currentUser.profileImageURL} alt={currentUser.username} className="rounded-circle p-1 " width="200" height="150"/>
+								}
+								<div className="nav-link mt-3" >
+									<h4 className='fw-bold'>{currentUser.username}</h4>
+									<div className="text-black mb-1">Owner of {currentUser.petdetails[0].petname}
+									</div>
 								</div>
                                 <button className='btn btn-danger p-2 mt-3 fw-bold' onClick={logout}>LOG OUT</button>
 							</div>
@@ -43,107 +47,79 @@ return (
 					</div>
 				</div>
 				<div className="col-lg-8 pt-2">
-					<div className="card m-3">
+					<div className="car m-3">
 						<div className="card-body">
-                            <h4 className='text-center fs-5'>USER DETAILS</h4>
-							<div className="row mb-3">
+                            <h4 className='text-center fs-5' id="pawscare">USER DETAILS</h4>
+							<div className="row mb-3 mt-4">
 								<div className="col-sm-3">
-									<h6 className="mb-0">Name:</h6>
+									<h6 className="mb-0" id="pawscare">Name:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.username}</p>
+                                    <p className='user-data'>{currentUser.username}</p>
 								</div>
 							</div>
 							<div className="row mb-3">
 								<div className="col-sm-3">
-									<h6 className="mb-0">Email:</h6>
+									<h6 className="mb-0" id="pawscare">Email:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.email}</p>
+                                    <p className='user-data'>{currentUser.email}</p>
 								</div>
 							</div>
 							<div className="row mb-3">
 								<div className="col-sm-3">
-									<h6 className="mb-0">Phone Number:</h6>
+									<h6 className="mb-0" id="pawscare">Phone Number:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.phonenumber}</p>
+                                    <p className='user-data'>{currentUser.phonenumber}</p>
 								</div>
 							</div>
 							<div className="row mb-3">
 								<div className="col-sm-3">
-									<h6 className="mb-0">Address</h6>
+									<h6 className="mb-0" id="pawscare">Address:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.address[0].addressline},{currentUser.address[0].district},{currentUser.address[0].state},{currentUser.address[0].country},{currentUser.address[0].pincode}</p>
+                                    <p className='user-data'>{currentUser.address[0].addressline}, {currentUser.address[0].district}, {currentUser.address[0].state}, {currentUser.address[0].country},   {currentUser.address[0].pincode}</p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-                <div className="col-lg-8 pt-2">
-					<div className="card m-3">
-						<div className="card-body">
-                            <h4 className='text-center fs-5'>PET DETAILS</h4>
-							<div className="row mb-3">
+                <div className="pt-2">
+					<div className=" m-3 ">
+                            <h4 className='text-center fs-5 mt-5' id="pawscare">PET DETAILS</h4>
+							<div className="row mb-3 mt-4">
 								<div className="col-sm-3">
-									<h6 className="mb-0">PetName:</h6>
+									<h6 className="mb-0" id="pawscare">Pet Name:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.petdetails[0].petname}</p>
-								</div>
-							</div>
-							<div className="row mb-3">
-								<div className="col-sm-3">
-									<h6 className="mb-0">ANIMAL:</h6>
-								</div>
-								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.petdetails[0].petanimal}</p>
+                                    <p className='user-data'>{currentUser.petdetails[0].petname}</p>
 								</div>
 							</div>
 							<div className="row mb-3">
 								<div className="col-sm-3">
-									<h6 className="mb-0">DATE OF BIRTH:</h6>
+									<h6 className="mb-0" id="pawscare">ANIMAL:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.petdetails[0].dob}</p>
+                                    <p className='user-data'>{currentUser.petdetails[0].petanimal}</p>
 								</div>
 							</div>
 							<div className="row mb-3">
 								<div className="col-sm-3">
-									<h6 className="mb-0">Previous Health Checkup:</h6>
+									<h6 className="mb-0" id="pawscare">DATE OF BIRTH:</h6>
 								</div>
 								<div className="col-sm-9 text-secondary">
-                                    <p className='form-control'>{currentUser.petdetails[0].last_checkup_date}</p>
+                                    <p className='user-data'>{currentUser.petdetails[0].dob}</p>
 								</div>
 							</div>
-						</div>
-					</div>
-				</div>
-                <div className="col-lg-4">
-					<div className="card m-3">
-						<div className="card-body">
-							<div className="d-flex flex-column align-items-center text-center">
-									<h4>Appointment Details</h4>
-								<div className="mt-3">
-                                    {currentUser.appointments?.service?
-                                    <div>
-                                        <p>SERVICE:
-                                            <span className='form-control'>{currentUser.appointments.service}</span>
-                                        </p>
-                                        <p>LOCATION:
-                                            <span className='form-control'>{currentUser.appointments.location}</span>
-                                        </p>
-                                        <p>DATE,TIME:
-                                            <span className='form-control'>{currentUser.appointments.date},{currentUser.appointments.time}</span>
-                                        </p>
-                                    </div>
-                                    :
-                                    <p className="text-black mb-1">No Appointment has been booked</p>
-                                    }
+							<div className="row mb-3">
+								<div className="col-sm-3">
+									<h6 className="mb-0" id="pawscare">Previous Health Checkup:</h6>
+								</div>
+								<div className="col-sm-9 text-secondary">
+                                    <p className='user-data'>{currentUser.petdetails[0].last_checkup_date}</p>
 								</div>
 							</div>
-						</div>
 					</div>
 				</div>
 			</div>
