317d539744464f8bb7e04850b68274f56b6397b2
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-12 23:46:25 +0530

    Update Wishlist.js

--- AppwithRedux/client/src/components/Wishlist.js
+++ AppwithRedux/client/src/components/Wishlist.js
@@ -4,6 +4,7 @@ import {useDispatch,useSelector} from 'react-redux'
 import axios from 'axios'
 import { productDetailsPromiseStatus } from '../redux/slices/productDetailsSlice' 
 import {Alert} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom'
 
 function Wishlist() {
 
@@ -73,7 +74,7 @@ async function openproductpage(item)
 {
     try
     {
-        dispatch(productDetailsPromiseStatus(item))
+        await dispatch(productDetailsPromiseStatus(item))
         navigate('/store/productpage')
     }
     catch(err)
@@ -108,38 +109,85 @@ return (
         <h1 className='text-center'>WISHLIST</h1>
         {error.length!==0&& <p className='fw-bold text-center text-danger border-0'>{error}</p>}
         {alert.length!==0 && <Alert variant={'dark'} onClose={()=>setAlert('')}>{alert}</Alert> }
-        <div className='container pt-2'>
-            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-4'>
+        <div className='row'>
+            {products.length>0&&
+            <>
             {products.map((item,index)=>
             (
-                <div className='col pt-3 pb-3'  key={index}>
-                    <div className='card' style={{height:'500px'}}>
+            <div className="col-md-3 col-sm-6 mt-4">
+            <div key={index} className="product-grid">
+                <div className="product-image">
+                    <a href="#" className="image">
                     {
-                        item.stock === 'In Stock' ?
-                        <img src={item.image} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name} onClick={()=>openproductpage(item)}/>
+                        item.stock === "In Stock"?
+                        <div>
+                        <img key={index} onClick={()=>openproductpage(item)} src={item.image[0].ImageURL}/>
+                        </div>
                         :
-                        <div className="product-card">
-                            <img src={item.image} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name} onClick={()=>openproductpage(item)}/>
-                            <div className={`text-overlay 'out-of-stock'}`}>
-                                <p>{'Out of Stock'}</p>
-                            </div>
+                        <div>
+                        <img key={index} onClick={()=>openproductpage(item)} src={item.image[0].ImageURL}/>
+                        <div className={`stock-overlay 'out-of-stock'}`}>
+                            <p className='fw-bold'>{'Out of Stock'}</p>
                         </div>
-                        }                        <div className='card-body'>
-                            <h5 className='card-title' onClick={()=>openproductpage(item)}>{item.productname}</h5>
-                            <p className='card-text'>Rs.{item.price}</p>
-                            <span>
-                            {
-                                item.stock === 'In Stock'?
-                                <button className='btn btn-success m-1' onClick={()=>addtocart(item)}>ADD TO CART</button>
-                                :
-                                <button className='btn btn-success m-1' disabled={true} onClick={()=>addtocart(item)}>ADD TO CART</button>
-                            }                            <button className='btn' onClick={()=>removefromwishlist(item)} ><span className='text-danger'><i class="bi bi-heart-fill"></i></span></button>    
-                            </span>
                         </div>
-                    </div>
+                    }
+                    </a>
+                    <span className="product-discount-label">-{item.discount_percent}%</span>
+                    <ul className="product-links">
+                        <li>
+                        <NavLink className='btn text-danger' onClick={()=>removefromwishlist(item)}><i class="bi bi-heart-fill"></i></NavLink>
+                        </li>
+                        {/* <li>
+                            <NavLink className='btn' onClick={()=>deleteproduct(item)}><i className="fa fa-trash"></i></NavLink>    
+                        </li> */}
+                    </ul>
+                {
+                    item.stock === 'In Stock'?
+                    <button className='add-to-cart' onClick={()=>addtocart(item)}>ADD TO CART</button>
+                    :
+                    <button className='add-to-cart-disabled' disabled={true} onClick={()=>addtocart(item)}>ADD TO CART</button>
+                }
+                    {/* <a href="" className="add-to-cart">ADD TO CART</a> */}
                 </div>
-            ))}
+                <div className="product-content" onClick={()=>openproductpage(item)}>
+                    <h3 className="title">
+                    <div className='btn' >
+                    {item.productname}
+                    </div>
+                    </h3>
+                    <div className="price">₹{item.discounted_price} <span>₹{item.price}</span></div>
+                </div>
             </div>
+        </div>
+                // <div className='col pt-3 pb-3'  key={index}>
+                //     <div className='card' style={{height:'500px'}}>
+                //     {
+                //         item.stock === 'In Stock' ?
+                //         <img src={item.image[0].ImageURL} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name} onClick={()=>openproductpage(item)}/>
+                //         :
+                //         <div className="product-card">
+                //             <img src={item.image[0].ImageURL} className='card-img-top' style={{width:'auto',height:'250px'}} alt={item.name} onClick={()=>openproductpage(item)}/>
+                //             <div className={`text-overlay 'out-of-stock'}`}>
+                //                 <p>{'Out of Stock'}</p>
+                //             </div>
+                //         </div>
+                //         }                        <div className='card-body'>
+                //             <h5 className='card-title' onClick={()=>openproductpage(item)}>{item.productname}</h5>
+                //             <p className='card-text'>Rs.{item.price}</p>
+                //             <span>
+                //             {
+                //                 item.stock === 'In Stock'?
+                //                 <button className='btn btn-success m-1' onClick={()=>addtocart(item)}>ADD TO CART</button>
+                //                 :
+                //                 <button className='btn btn-success m-1' disabled={true} onClick={()=>addtocart(item)}>ADD TO CART</button>
+                //             }                            <button className='btn' onClick={()=>removefromwishlist(item)} ><span className='text-danger'><i class="bi bi-heart-fill"></i></span></button>    
+                //             </span>
+                //         </div>
+                //     </div>
+                // </div>
+            ))}
+            </>
+            }
         </div>
     </div>
 )
