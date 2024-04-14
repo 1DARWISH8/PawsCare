8445bfd3298b1a2a655a9e04f475d1fb7e9232cd
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-14 17:46:25 +0530

    Update Checkout.js

--- AppwithRedux/client/src/components/Checkout.js
+++ AppwithRedux/client/src/components/Checkout.js
@@ -1,10 +1,8 @@
 import React, { useContext, useState } from 'react'
-import { userLoginContext } from '../contexts/userLoginContext'
 import { NavLink } from 'react-router-dom'
 
 function Checkout() {
 
-  let [currentUser]=useContext(userLoginContext)
 
   return (
     <div>
@@ -12,8 +10,8 @@ function Checkout() {
         <NavLink className='bg-secondary btn text-white' to='/store/cart'>ᐊ Back</NavLink>
       </div>
       <div className='card text-center pt-5'>
-        <h3 className='fw-bold'>{currentUser.username},Thank You for placing an Order</h3>
-        <p>Your Order will be Delivered within 7 Working days to the {currentUser.address}</p>
+        <h3 className='fw-bold'>,Thank You for placing an Order</h3>
+        <p>Your Order will be Delivered within 7 Working days to the</p>
     </div>
     </div>
   )
