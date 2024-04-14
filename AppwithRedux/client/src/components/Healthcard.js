170fb4f09e0d282af17ed3e8f5ed66f84ecf5c61
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-14 17:48:01 +0530

    Update Healthcard.js

--- AppwithRedux/client/src/components/Healthcard.js
+++ AppwithRedux/client/src/components/Healthcard.js
@@ -1,11 +1,9 @@
 // useContext hook is imported to change 
 import React, { useContext } from 'react'
 
-import { userLoginContext } from '../contexts/userLoginContext'
 
 function Healthcard() {
 
-    let [currentUser]=useContext(userLoginContext)
     
 return (
     <div className='container'>
@@ -13,14 +11,14 @@ return (
         <div className='col-12'>
             <div className=' card bg-success m-5  g-0 text-center'>
             <div >
-                <img src={currentUser.imageupload} className='img-fluid rounded-circle m-2' width="150" height="150" alt='PET'/>
+                <img src="" className='img-fluid rounded-circle m-2' width="150" height="150" alt='PET'/>
             </div>
                 <div className='card-body text-light fw-bold '>
-                    <p>PETNAME:<span className='text-black m-2'>{currentUser.petname}</span></p>
-                    <p>Previous health checkup was on <span className='fw-bold text-black m-1'>"{currentUser.checkupdate}"</span></p>
-                    {currentUser.date?<p>Next appointment has been booked for <span className='text-black'>"{currentUser.date}"</span></p>:
-                        <p className='text-warning'>Schedule {currentUser.petname}'s next health checkup in the next 2 months</p>
-                    }
+                    <p>PETNAME:<span className='text-black m-2'></span></p>
+                    <p>Previous health checkup was on <span className='fw-bold text-black m-1'>""</span></p>
+                    {/* {currentUser.date?<p>Next appointment has been booked for <span className='text-black'>""</span></p>: */}
+                        <p className='text-warning'>Schedule 's next health checkup in the next 2 months</p>
+                    {/* } */}
                 </div>
             </div>
         </div>
