22545b672ed06f1f91f1608f927d7581876f39c1
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-26 23:08:55 +0530

    Update Contact.js

--- AppwithRedux/client/src/components/Contact.js
+++ AppwithRedux/client/src/components/Contact.js
@@ -1,51 +1,87 @@
 import React from 'react'
-// import styled from 'styled-components'
-
-
-// let backgroundImage = 'url("https://img.freepik.com/free-vector/frame-with-dogs-vector-white-background_53876-127700.jpg")';
-
-// let Background = styled.div`
-// background-image: ${backgroundImage};
-// background-size: cover;
-// background-position: center;
-// width: 100vw;
-// height: 100vh;
-// position: relative;
-// `;
-
-// const Content = styled.div`
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   text-align: center;
-//   color: white;
-// `;
 
 function Contact() {
   let logo = 'https://res.cloudinary.com/dozacgfl7/image/upload/v1711878050/logo_with_name_l2vh5v.png'
   return (
-    <div>
-      <div className='card-group p-5'>
-      <div className='card border-0' >
-        <div className='card-body justify-content-center text-center'>
-          <h3>CONTACT US</h3>
-          <img src={logo} alt='logo'></img>
+    <section>
+        <div className='row p-3'>
+            <div className='col-12 col-xl-6'>
+              <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714114702/canvagetintouch_zcagzh.png' style={{ width: '100%'}}></img>
+            </div>
+            <div className='col-12 col-xl-6 my-auto'>
+              <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714118383/contactblocks_riyniq.jpg' style={{ width: '100%'}}></img>
+            </div>
+        </div>
+      
+      <h1 id='health' className='p-4 fs-1'>VISIT US AT</h1>
+
+      <div className='row p-4'>
+        <div className='col-12 col-xl-6'>
+          <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714113122/dog_with_phone_zwdym4.jpg' style={{ width: '100%'}}></img>
+        </div>
+        <div className='col-12 col-xl-6 my-auto'>
+          <div>
+            <span id='pawscare' className='fs-2'>PAWSCARE BENGALURU</span>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Email:<span id='pawscare' className='fs-5'> pawscare.bengaluru@gmail.com</span></strong>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Phone:<span id='pawscare' className='fs-5'> +91 7896541235</span></strong>
+          </div>
         </div>
       </div>
-      <div className='card border-0'style={{width:'300px',Height:'500px'}} >
-        <div className='card-body pt-5'>
-          <h5>PHONE:</h5>
-          <p>+91 85984 49030 (BANGALORE)</p>
-          <p>+91 93829 29239 (CHENNAI) </p>
-          <p>+91 48833 98437 (DELHI)</p>
-          <p>+91 95436 64365 (HYDERABAD) </p>
-          <h6>WRITE TO US:</h6>
-          <a className='fw-bold' href='#'>pawscare@gmail.com</a>
+      <div className='row p-4'>
+      <div className='col-12 col-xl-6 my-auto'>
+          <div>
+            <span id='pawscare' className='fs-2'>PAWSCARE CHENNAI</span>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Email:<span id='pawscare' className='fs-5'> pawscare.chennai@gmail.com</span></strong>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Phone:<span id='pawscare' className='fs-5'> +91 8527531594</span></strong>
+          </div>
+        </div>
+        <div className='col-12 col-xl-6'>
+          <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714113122/dalm_with_phone_pewibi.jpg' style={{ width: '100%'}}></img>
+        </div>
+
+      </div>
+      <div className='row p-4'>
+        <div className='col-12 col-xl-6'>
+          <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714118831/cat_with_phone_eu5bwy.jpg' style={{ width: '100%'}}></img>
+        </div>
+        <div className='col-12 col-xl-6 my-auto'>
+          <div>
+            <span id='pawscare' className='fs-2'>PAWSCARE HYDERABAD</span>
+          </div>
+          <div >
+            <strong id='health' className='fs-4'>Email:<span id='pawscare' className='fs-5'> pawscare.hyderabad@gmail.com</span></strong>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Phone:<span id='pawscare' className='fs-5'> +91 9632581475</span></strong>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
+      <div className='row p-4'>
+        <div className='col-12 col-xl-6 my-auto'>
+          <div>
+            <span id='pawscare' className='fs-2'>PAWSCARE VISAKHAPATNAM</span>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Email:<span id='pawscare' className='fs-5'> pawscare.vizag@gmail.com</span></strong>
+          </div>
+          <div>
+            <strong id='health' className='fs-4'>Phone:<span id='pawscare' className='fs-5'> +91 8524561298</span></strong>
+          </div>
+        </div>
+        <div className='col-12 col-xl-6'>
+          <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714118831/cat_wit_phone_r5bqsk.jpg' style={{ width: '100%'}}></img>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
