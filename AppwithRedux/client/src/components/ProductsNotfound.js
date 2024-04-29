0ee65b146fb665fb76bea7e0eec69d2cfe2190da
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-04-29 15:45:38 +0530

    Update ProductsNotfound.js

--- AppwithRedux/client/src/components/ProductsNotfound.js
+++ AppwithRedux/client/src/components/ProductsNotfound.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 function ProductsNotfound({message}) {
+
+  const imgWidth = window.innerWidth >= 768 ? '40%' : '95%';
+  
   return (
     <div className='my-auto text-center p-3'>
         <h4 className='text-danger' id='health'>OOPS!</h4>
         <h5 id='health'>{message}</h5>
-        <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714370673/unable_to_find_pa9trp.jpg' width='95%'/>
+        <img src='https://res.cloudinary.com/dozacgfl7/image/upload/v1714370673/unable_to_find_pa9trp.jpg' width={imgWidth}/>
     </div>
   )
 }
